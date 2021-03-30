9847a70bda8a182f9da1834d3ab765c66d85a150
T-Shikai <tatsuroshikai@gmail.com>
2021-03-30 03:25:52 +0000

    fix topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,4 @@ class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :books, dependent: :destroy
   validates :title, { presence: true, length: { maximum: 100 } }
-  validates :status, presence: true
 end
