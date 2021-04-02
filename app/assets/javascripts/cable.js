34c4c0f35576049ad6efce21e9b6d91f4a61fcbb
T-Shikai <tatsuroshikai@gmail.com>
2021-04-02 14:54:12 +0000

    add room_channel

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -4,7 +4,7 @@
 //= require action_cable
 //= require_self
 //= require_tree ./channels
-
+/* global ActionCable */
 (function() {
   this.App || (this.App = {});
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
   root to: 'homes#top'
   get 'about' => 'homes#about'
 
